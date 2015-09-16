cec715759541998984bbcad9f2702b175b97ffef
felipeue <felipe.rios@mail.udp.cl>
2015-09-16 16:08:43 -0300

    First map view

--- principal/views.py
+++ principal/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, render_to_response
 from django.template.context import RequestContext
+from principal.models import Post
 from django.http import HttpResponse
 
 
 def index(request):
+    posts = Post.objects.get(id=1)
+    points = posts.location
     context = RequestContext(request,
                              {'request': request,
-                              'user': request.user})
+                              'user': request.user,
+                              'qs_results': points})
     return render_to_response('index.html', context_instance=context)
 
 
