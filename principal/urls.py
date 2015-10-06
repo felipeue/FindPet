04d95f3818a03330c3d3abc63bc764b05958ba3b
felipeue <felipe.rios@mail.udp.cl>
2015-10-06 15:01:00 -0300

    Register and login

--- principal/urls.py
+++ principal/urls.py
@@ -3,5 +3,6 @@ from principal import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-
+                       url(r'^register/$', views.register, name='register'),
+                       url(r'^login/$', views.user_login, name='login'),
                        )
--- principal/views.py
+++ principal/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from principal.models import Post
-from django.http import HttpResponse
-import json
+from principal.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
@@ -10,3 +11,43 @@ def index(request):
     return render(request, 'index.html', {'points': points})
 
 
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            registered = True
+        else:
+            print user_form.errors, profile_form.errors
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render(request,
+                  'register.html',
+                  {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/principal/')
+            else:
+                return HttpResponse("You account is disabled")
+        else:
+            print "invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse('Invalid login details')
+    else:
+        return render(request, 'login.html', {})
