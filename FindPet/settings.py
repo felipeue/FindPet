aac226cabc466f15862f9056e6ea6eb0fd37bed6
felipeue <felipe.rios@mail.udp.cl>
2015-12-11 15:55:50 -0300

    production commit

--- FindPet/settings.py
+++ FindPet/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9$19q-^g&@0*^=2os*_7#6wp6=sh-wgov(iw4ymgclc3^ku)(y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
