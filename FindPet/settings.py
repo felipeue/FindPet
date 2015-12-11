e389f652bb2630f5fcd5b7b76f01e955860a2e3c
felipeue <felipe.rios@mail.udp.cl>
2015-12-11 15:57:54 -0300

    production commit

--- FindPet/settings.py
+++ FindPet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9$19q-^g&@0*^=2os*_7#6wp6=sh-wgov(iw4ymgclc3^ku)(y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
